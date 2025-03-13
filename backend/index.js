6bf682a3116f9c5d5fbad705143470f17423a60e
jaiganesh5555 <142227919+jaiganesh5555@users.noreply.github.com>
2025-03-13 09:59:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const mainRouter = require("./routes/index");
 
 const app = express();
 app.use(cors());
+const corsConfig={
+    origin="*",
+    credential:true,
+    methods:["GET","POST","PUT","DELETE"],
+};
+app.use(cors(corsConfig));
 app.use(express.json());
 const PORT = 3000;
 
